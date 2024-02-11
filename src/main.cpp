a192bbec5ed422b9d25c9c254070f093a4344ff6
Aleksey Baranov <99679510+EmbedDevIOT@users.noreply.github.com>
2024-02-11 22:15:25 +0300

    v.0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <GyverButton.h>
+#include <EncButton.h>
 #include <GyverStepper.h>
 
 #define speed 115200
@@ -13,13 +13,34 @@
 #define RL3 17
 #define RL4 16
 
-#define BTN 34
+#define BTN 33
 #define SEN1 32
 #define SEN2 35
 
-GButton button(BTN);
+Button btnUSER(BTN, INPUT_PULLUP);
 
-char fw = {0.1};
+String fw = "0.2";
+
+uint8_t state = 0b00000000;
+uint8_t count = 0;
+
+enum ActState
+{
+  Act_OFF = 0b00000000,
+  Act1_UP = 0b00000001,
+  Act1_DWN = 0b00000010,
+  Act2_UP = 0b00000100,
+  Act2_DWN = 0b00001000
+};
+struct FLAG
+{
+  bool ACT1 = 0;
+  bool ACT2 = 0;
+};
+
+void SetStateRelay(uint8_t st);
+void ButtonHandler();
+void task_1000();
 
 void setup()
 {
@@ -35,11 +56,127 @@ void setup()
   pinMode(RL4, OUTPUT);
   digitalWrite(RL4, HIGH);
 
-  pinMode(SEN1, INPUT);
-  pinMode(SEN2, INPUT);
-
+  pinMode(SEN1, INPUT_PULLUP);
+  pinMode(SEN2, INPUT_PULLUP);
 }
 
 void loop()
 {
+  ButtonHandler();
+  task_1000();
+}
+
+void ButtonHandler()
+{
+  static uint8_t step_count = 0;
+  uint32_t now;
+
+  btnUSER.tick();
+
+  while (btnUSER.busy())
+  {
+    btnUSER.tick();
+
+    if (btnUSER.hasClicks(1))
+    {
+      Serial.println("RL4 ON");
+      if (state != 0b00001000)
+      {
+        state = 0b00001000;
+      }
+      else
+        state = 0b00000100;
+
+      // now = millis();
+      // while (millis() - now < 10000)
+      // {
+      // }
+
+      // bitClear(state, 0);
+      // bitClear(state, 1);
+    }
+
+    if (btnUSER.hasClicks(2))
+    {
+      Serial.println("RL 1 ON RL 2 OFF");
+      bitSet(state, 0);
+      bitClear(state, 1);
+    }
+
+    if (btnUSER.hasClicks(3))
+    {
+      Serial.println("RL 1 OFF RL 2 ON");
+      bitClear(state, 0);
+      bitSet(state, 1);
+    }
+  }
+}
+
+void task_1000()
+{
+  static uint32_t timer_1000 = 0;
+
+  char msg[30];
+
+  if (millis() - timer_1000 > 1000)
+  {
+    timer_1000 = millis();
+    sprintf(msg, "Timer 1000");
+    Serial.println(msg);
+
+    if (!digitalRead(SEN1))
+    {
+      sprintf(msg, "SENSOR_ 1 Activate");
+      Serial.println(msg);
+    }
+
+    if (!digitalRead(SEN2))
+    {
+      sprintf(msg, "SENSOR_ 2 Activate");
+      Serial.println(msg);
+    }
+
+    SetStateRelay(state);
+
+    if ((state == 0b00000100) || (state == 0b00001000))
+    {
+      count++;
+      Serial.println(count);
+      if (count == 10)
+      {
+        count = 0;
+        state = 0b00000000;
+      }
+    }
+  }
+}
+
+void SetStateRelay(uint8_t st)
+{
+  switch (st)
+  {
+  case 0b00000000:
+    digitalWrite(RL1, HIGH);
+    digitalWrite(RL2, HIGH);
+    digitalWrite(RL3, HIGH);
+    digitalWrite(RL4, HIGH);
+    break;
+  case 0b00000001:
+    digitalWrite(RL1, LOW);
+    digitalWrite(RL2, HIGH);
+    break;
+  case 0b00000010:
+    digitalWrite(RL1, HIGH);
+    digitalWrite(RL2, LOW);
+    break;
+  case 0b00000100:
+    digitalWrite(RL3, LOW);
+    digitalWrite(RL4, HIGH);
+    break;
+  case 0b00001000:
+    digitalWrite(RL3, HIGH);
+    digitalWrite(RL4, LOW);
+  default:
+    break;
+  }
 }
