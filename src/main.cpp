ca6c545539ed3550fb1ab0a233683036fe8327ad
EmbedDevIOT <Embeddev.IOT@gmail.com>
2024-02-12 12:59:07 +0300

    v.0.4 SMot_EN

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,17 @@
 Button btnUSER(BTN, INPUT_PULLUP);
 GStepper<STEPPER2WIRE> stepper(1600, PUL, DIR, ENA); // драйвер step-dir + пин enable
 
-String fw = "0.2";
+String fw = "0.4";
 
 uint8_t state = 0b00000000;
 uint8_t count = 0;
+
+enum state
+{
+  NONE,
+  OPEN,
+  CLOSE
+};
 
 enum ActState
 {
@@ -33,14 +40,21 @@ enum ActState
   Act2_UP = 0b00000100,
   Act2_DWN = 0b00001000
 };
+
 struct FLAG
 {
   bool ACT1 = 0;
   bool ACT2 = 0;
+  bool HS1 = 0;
+  bool HS2 = 0;
+  uint8_t Table = 0;
 };
+
+struct FLAG FState;
 
 void SetStateRelay(uint8_t st);
 void ButtonHandler();
+void TableConroller();
 void task_1000();
 
 void setup()
@@ -60,9 +74,8 @@ void setup()
   pinMode(SEN1, INPUT_PULLUP);
   pinMode(SEN2, INPUT_PULLUP);
 
-  stepper.setMaxSpeed(800);
-  stepper.setAcceleration(300);
-  stepper.setRunMode(FOLLOW_POS);
+  stepper.setRunMode(KEEP_SPEED);
+
   stepper.disable();
   stepper.stop();
   stepper.reset();
@@ -72,16 +85,33 @@ void setup()
 
 void loop()
 {
-  // просто крутим туды-сюды
-  // if (!stepper.tick())
-  // {
-  //   static bool dir;
-  //   dir = !dir;
-  //   stepper.setTarget(dir ? -1024 : 1024);
-  // }
-  stepper.tick();
+  if (digitalRead(SEN1))
+  {
+    FState.HS1 = 1;
+    stepper.setAcceleration(300);
+    stepper.setSpeedDeg(600);
+    stepper.enable();
+    // stepper.setMaxSpeed(800);
+
+    Serial.println("S1: ON");
+  }
+
+  while (FState.HS1)
+  {
+    stepper.tick();
+
+    if (digitalRead(SEN2))
+    {
+      Serial.println("S2: ON");
+      FState.HS1 = 0;
+      stepper.disable();
+      stepper.stop();
+      stepper.reset();
+    }
+  }
 
   ButtonHandler();
+  TableConroller();
   task_1000();
 }
 
@@ -98,15 +128,19 @@ void ButtonHandler()
 
     if (btnUSER.hasClicks(1))
     {
-      Serial.println("RL4 ON");
-      stepper.setTargetDeg(360);
-      stepper.enable();
-      if (state != 0b00001000)
+      if (FState.Table == NONE || FState.Table == CLOSE)
       {
-        state = 0b00001000;
+        FState.Table = OPEN;
       }
-      else
-        state = 0b00000100;
+
+      // stepper.setTargetDeg(360);
+      // stepper.enable();
+      // if (state != 0b00001000)
+      // {
+      //   state = 0b00001000;
+      // }
+      // else
+      //   state = 0b00000100;
 
       // now = millis();
       // while (millis() - now < 10000)
@@ -119,16 +153,17 @@ void ButtonHandler()
 
     if (btnUSER.hasClicks(2))
     {
-      Serial.println("RL 1 ON RL 2 OFF");
-      bitSet(state, 0);
-      bitClear(state, 1);
+      if (FState.Table == NONE || FState.Table == OPEN)
+      {
+        FState.Table = CLOSE;
+      }
+      // Serial.println("RL 1 ON RL 2 OFF");
+      // bitSet(state, 0);
+      // bitClear(state, 1);
     }
 
     if (btnUSER.hasClicks(3))
     {
-      Serial.println("RL 1 OFF RL 2 ON");
-      bitClear(state, 0);
-      bitSet(state, 1);
     }
   }
 }
@@ -142,13 +177,31 @@ void task_1000()
   if (millis() - timer_1000 > 1000)
   {
     timer_1000 = millis();
-    sprintf(msg, "Timer 1000");
+    sprintf(msg, "T: 1000");
     Serial.println(msg);
+  }
+}
+
+void TableConroller()
+{
+  char msg[30];
+
+  if (FState.Table == OPEN)
+  {
+    Serial.println("Table Opening");
+    delay(3000);
+    Serial.println("Driver 1 UP");
+    SetStateRelay(Act2_UP);
+    delay(10000);
 
     if (digitalRead(SEN1))
     {
       sprintf(msg, "SENSOR_ 1 Activate");
       Serial.println(msg);
+
+      Serial.println("Motor Enable");
+      stepper.setTarget(360);
+      stepper.enable();
     }
 
     if (digitalRead(SEN2))
@@ -156,23 +209,52 @@ void task_1000()
       sprintf(msg, "SENSOR_ 2 Activate");
       Serial.println(msg);
     }
+    Serial.println("Driver 2 UP");
+    SetStateRelay(Act1_UP);
+    delay(10000);
 
-    SetStateRelay(state);
+    FState.Table = NONE;
+    Serial.println("Driver OFF");
+  }
+  else if (FState.Table == CLOSE)
+  {
+    Serial.println("Table Closing");
+    delay(3000);
+    Serial.println("Driver 1 DWN");
+    SetStateRelay(Act2_DWN);
+    delay(10000);
 
-    if ((state == 0b00000100) || (state == 0b00001000))
+    if (digitalRead(SEN1))
     {
-      count++;
-      Serial.println(count);
+      sprintf(msg, "SENSOR_ 1 Activate");
+      Serial.println(msg);
 
-      if (count == 10)
-      {
-        count = 0;
-        state = 0b00000000;
-        stepper.disable();
-        stepper.stop();
-        stepper.reset();
-      }
+      Serial.println("Motor Enable");
+      stepper.setTarget(360);
+      stepper.enable();
     }
+
+    if (digitalRead(SEN2))
+    {
+      sprintf(msg, "SENSOR_ 2 Activate");
+      Serial.println(msg);
+    }
+    Serial.println("Driver 2 DWN");
+    SetStateRelay(Act1_DWN);
+    delay(10000);
+
+    FState.Table = NONE;
+    Serial.println("Driver OFF");
+  }
+  else
+  {
+    FState.Table = NONE;
+
+    stepper.disable();
+    stepper.stop();
+    stepper.reset();
+
+    SetStateRelay(Act_OFF);
   }
 }
 
